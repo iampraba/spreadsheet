66360c4f9b59d1254bcabf8532f47c02c20b9407
Prabakaran R <prabakaran.r@zohocorp.com>
2020-12-08 22:41:38 +0530

    Comments added for clarity

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
 # spreadsheet
 
-This repoistory is used to demonstrate how to generate & redirect  spreadsheet using office integrator api's.
+This repository is used to demonstrate how to generate & redirect spreadsheet using office integrator api's.
 
-Run following command to run the application
+Run following command to run the application in your machine.
 
+```sh
 git clone https://github.com/iampraba/spreadsheet.git
+```
 
+```sh
 cd spreadsheet
+```
 
+```sh
 npm install
+```
 
-node index.js
-
+```sh
+npm start
+```
 
 Once server is started, user below url to generate a spreadsheet.
 
-To Create a new - http://localhost:3000/spreadsheet/new
+To create a new spreadsheet - http://localhost:3000/spreadsheet/new
 
-To create multiple session for same spreadsheet, use a document in the url. 
+To create multiple session for same spreadsheet, use same `spreadsheet-id` in below url. 
 
 i.e - http://localhost:3000/spreadsheet/<spreadsheet-id> 
 
 e.x: http://localhost:3000/spreadsheet/10000
+
+Live Example: 
+
+To create a new spreadsheet - https://sheetg-699959938.development.zohocatalyst.com/server/spreadsheet/new
+
+To access a existing spreadsheet - https://sheetg-699959938.development.zohocatalyst.com/server/spreadsheet/10000
--- index.js
+++ index.js
@@ -13,39 +13,43 @@ const config = {
     }
 };
 
+//Method returns promise object of spreadsheet session create request.
 const createSheetSession = (document_info, req, res) => {
-	var data = {};
-	
-	data.document_info = JSON.stringify(document_info);
+    var data = {};
 
-	return axios({
-	  method: 'post',
-	  data: qs.stringify(data),
-	  url: api_enpoint + "?apikey=" + apikey,
-	  responseType: 'json'
-	});
+    data.document_info = JSON.stringify(document_info);
+
+    return axios({
+      method: 'post',
+      data: qs.stringify(data),
+      url: api_enpoint + "?apikey=" + apikey,
+      responseType: 'json'
+    });
 };
 
+//Once spreadsheet session created, this method will redirect to user to generated spread sheet session
 const onSessionCreateSuccess = ( response, responseObject ) => {
-	// handle success
+    // handle success
     var status = response.status;
 
     if ( status == 200 ) {
-    	var redirect_url = response.data.gridview_url || response.data.document_url;
+        var redirect_url = response.data.gridview_url || response.data.document_url;
 
-    	responseObject.writeHead(307, {
-	    	Location: redirect_url
-	    });
-	    console.log("Redirect Url: ", redirect_url);
-	    console.log("response : ", response.data);
+        responseObject.writeHead(307, {
+            Location: redirect_url
+        });
+        console.log("Redirect Url: ", redirect_url);
+    } else {
+        responseObject.status(500).send("Spreadsheet creation request failed");
     }
     responseObject.end();
-    
 };
 
+//Once spreadsheet session created, this method will redirect to user to generated spread sheet session
 const onSessionCreateFailure = ( error, responseObject ) => {
-	// handle error
+    // handle error
     console.log(error);
+    responseObject.status(500).send("Spreadsheet creation request failed");
     responseObject.end();
 };
 
@@ -54,37 +58,41 @@ const app = express();
 app.use(express.json());
 
 app.all("/",(req, res) => {
-	res.status(200).send("I am Live and Ready.");
+    res.status(200).send("I am Live and Ready.");
 });
 
+//This api end point will create a new spreadsheet. 
+//User will be redirected to created session url as response.
 app.all("/spreadsheet/new",(req, res) => {
-	let spreadsheetId = new Date().getTime();
+    let spreadsheetId = new Date().getTime();
 
-	var promise = createSheetSession({
-		document_id: spreadsheetId,
-		document_name: "New Spreadsheet"
-	});
+    var promise = createSheetSession({
+        document_id: spreadsheetId,
+        document_name: "New Spreadsheet"
+    });
 
-	promise.then(function(response) {
-    	onSessionCreateSuccess(response, res);
-	}).catch(function (error) {
-	    onSessionCreateFailure(error, res);
-	});
+    promise.then(function(response) {
+        onSessionCreateSuccess(response, res);
+    }).catch(function (error) {
+        onSessionCreateFailure(error, res);
+    });
 });
 
+//This api end point will create new spreadsheet with given 'spreadsheetId' in api end point url.
+//User will be redirected to created session url as response.
 app.get("/spreadsheet/:spreadsheetId", (req,res) => {
-	let spreadsheetId = req.params.spreadsheetId;
+    let spreadsheetId = req.params.spreadsheetId;
 
-	var promise = createSheetSession({
-		document_id: spreadsheetId,
-		document_name: "Existing Spreadsheet"
-	});
+    var promise = createSheetSession({
+        document_id: spreadsheetId,
+        document_name: "Existing Spreadsheet"
+    });
 
-	promise.then(function(response) {
-    	onSessionCreateSuccess(response, res);
-	}).catch(function (error) {
-	    onSessionCreateFailure(error, res);
-	});
+    promise.then(function(response) {
+        onSessionCreateSuccess(response, res);
+    }).catch(function (error) {
+        onSessionCreateFailure(error, res);
+    });
 
 });
 
@@ -92,6 +100,6 @@ var port = 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 
 module.exports = app;
